6d1154ef2c90201c10b8598873456f52e71dd6ee
Benjamin Axline <baxline@bu.edu>
2024-09-15 18:00:19 -0400

    sound

--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -1,48 +1,102 @@
-#!/usr/bin/env python3
 """
-PWM Tone Generator
-
-based on https://www.coderdojotc.org/micropython/sound/04-play-scale/
+Response time - single-threaded
 """
 
-import machine
-import utime
-
-# GP16 is the speaker pin
-SPEAKER_PIN = 16
-
-# create a Pulse Width Modulation Object on this pin
-speaker = machine.PWM(machine.Pin(SPEAKER_PIN))
+from machine import Pin
+import time
+import random
+import json
 
 
-def playtone(frequency: float, duration: float) -> None:
-    speaker.duty_u16(1000)
-    speaker.freq(int(frequency))
-    utime.sleep(duration)
+N: int = 3
+sample_ms = 10.0
+on_ms = 500
 
 
-def quiet():
-    speaker.duty_u16(0)
-
-notes={
-    "C5":523.25,
-    "B4":466.16,
-    "A4":415.30,
-    "G4":392.00
-    }
+def random_time_interval(tmin: float, tmax: float) -> float:
+    """return a random time interval between max and min"""
+    return random.uniform(tmin, tmax)
 
 
-melody = [
-    ("C5",0.5), ("B4", 0.5), ("A4", 0.5), ("G4",0.5), ("G4", 0.3), ("A4", 0.3),
-    ("A4", 0.3), ("A4", 0.3), ("G4", 0.3), ("A4", 0.3), ("A4", 0.3), ("A4", 0.3)
-    ]
+def blinker(N: int, led: Pin) -> None:
+    # %% let user know game started / is over
 
-print("Playing frequency (Hz):")
-
-for note, duration in melody:
-    playtone(notes[note], duration)
-
+    for _ in range(N):
+        led.high()
+        time.sleep(0.1)
+        led.low()
+        time.sleep(0.1)
 
 
-# Turn off the PWM
-quiet()
+def write_json(json_filename: str, data: dict) -> None:
+    """Writes data to a JSON file.
+
+    Parameters
+    ----------
+
+    json_filename: str
+        The name of the file to write to. This will overwrite any existing file.
+
+    data: dict
+        Dictionary data to write to the file.
+    """
+
+    with open(json_filename, "w") as f:
+        json.dump(data, f)
+
+
+def scorer(t: list[int | None]) -> None:
+    # %% collate results
+    misses = t.count(None)
+    print(f"You missed the light {misses} / {len(t)} times")
+
+    t_good = [x for x in t if x is not None]
+
+    print(t_good)
+
+    # add key, value to this dict to store the minimum, maximum, average response time
+    # and score (non-misses / total flashes) i.e. the score a floating point number
+    # is in range [0..1]
+    data = {}
+
+    # %% make dynamic filename and write JSON
+
+    now: tuple[int] = time.localtime()
+
+    now_str = "-".join(map(str, now[:3])) + "T" + "_".join(map(str, now[3:6]))
+    filename = f"score-{now_str}.json"
+
+    print("write", filename)
+
+    write_json(filename, data)
+
+
+if __name__ == "__main__":
+    # using "if __name__" allows us to reuse functions in other script files
+
+    led = Pin("LED", Pin.OUT)
+    button = Pin(16, Pin.IN, Pin.PULL_UP)
+
+    t: list[int | None] = []
+
+    blinker(3, led)
+
+    for i in range(N):
+        time.sleep(random_time_interval(0.5, 5.0))
+
+        led.high()
+
+        tic = time.ticks_ms()
+        t0 = None
+        while time.ticks_diff(time.ticks_ms(), tic) < on_ms:
+            if button.value() == 0:
+                t0 = time.ticks_diff(time.ticks_ms(), tic)
+                led.low()
+                break
+        t.append(t0)
+
+        led.low()
+
+    blinker(5, led)
+
+    scorer(t)
